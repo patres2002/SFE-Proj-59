c2cdf66b77725b14db4c3b9eb84c235f3148a347
comfy332 <eitanabelman@gmail.com>
2023-04-04 22:02:34 +0100

    Began making admin changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ conn = sqlite3.connect('system.db')
 c = conn.cursor()
 
 # Create the users table if it doesn't exist
-c.execute('''CREATE TABLE IF NOT EXISTS users (username text, password text)''')
+c.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)''')
 
 
 # Check if the table is empty
@@ -19,12 +19,23 @@ row_count = c.fetchone()[0]
 
 # Insert example data only if the table is empty
 if row_count == 0:
-    c.execute("INSERT INTO users VALUES ('admin', 'admin')")
-    c.execute("INSERT INTO users VALUES ('student', 'student')")
+    c.execute("INSERT INTO users (username, password) VALUES ('admin', 'admin')")
+    c.execute("INSERT INTO users (username, password) VALUES ('student', 'student')")
     conn.commit()
 
 # Create the tickets table for issues
 c.execute('''CREATE TABLE IF NOT EXISTS tickets (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    title TEXT NOT NULL,
+    description TEXT,
+    status TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+# Create the ecs table for ECs
+c.execute('''CREATE TABLE IF NOT EXISTS ecs (
     id INTEGER PRIMARY KEY,
     user_id INTEGER,
     title TEXT NOT NULL,
@@ -39,47 +50,65 @@ conn.close()
 
 # Login page, or home page if logged in
 @app.route('/', methods=['GET', 'POST'])
-def login():
+def login() -> str:
     if 'username' in session:
         return redirect(url_for('home'))
+
     if request.method == 'POST':
-        # print("about to post")
-        # Connect to the database
-        conn = sqlite3.connect('system.db')
-        # print("connected")
-        c = conn.cursor()
+        # try:
+            # Connect to the database
+            conn = sqlite3.connect('system.db')
+            c = conn.cursor()
 
-        # Get the details from the form
-        # print("getting the details")
-        username = request.form['username']
-        password = request.form['password']
-        # print(username, password)
-        # Check if the details are correct
-        c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
-        data = c.fetchone()
+            # Get the details from the form
+            username = request.form['username']
+            password = request.form['password']
 
-        # If the details are correct, log the user in
-        if data is not None:
-            session['username'] = request.form['username']
-            # print("here")
-            return redirect(url_for('home'))
+            # Check if the details are correct
+            c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+            data = c.fetchone()
 
-        # Close the database
-        conn.close()
+            # If the details are correct, log the user in
+            if data is not None:
+                session['username'] = request.form['username']
+                # Store the user ID in the session
+                session['user_id'] = data[0]
+                return redirect(url_for('home'))
+
+            # Close the database
+            conn.close()
+        # except Exception as e:
+        #     # Handle errors
+        #     return render_template('error.html', message="An error occurred while accessing the database.")
 
     # If the details are incorrect, show the login page
     return render_template('login.html')
 
 # Home page
 @app.route('/home')
-def home():
+def home() -> str:
     if 'username' in session:
-        return render_template('home.html', username = session['username'])
+        # Check if the logged in user is an admin
+        if session['username'] == 'admin':
+            return render_template('admin.html', username=session['username'])
+        else:
+            return render_template('home.html', username=session['username'])
     return redirect(url_for('login'))
 
 # EC page
-@app.route('/ec')
+@app.route('/ec', methods=['GET', 'POST'])
 def ec():
+    if request.method == 'POST':
+        # print("about to post")
+        # Connect to the database
+        conn = sqlite3.connect('system.db')
+        # print("connected")
+        c = conn.cursor()
+        c.execute("INSERT INTO ecs (user_id, title, description, status) VALUES (?, ?, ?, ?)", ())
+        data = c.fetchone()
+        # Close the database
+        conn.close()
+
     if 'username' in session:
         return render_template('ec.html', username = session['username'])
     return redirect(url_for('login'))
